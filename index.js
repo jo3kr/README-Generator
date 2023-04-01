49b684c6ff7d919d36a9b0de20ff989641b3de02
jo3kr <joelmjohn16@gmail.com>
2023-04-01 11:38:12 -0500

    added required packages, wrote out questions array, wrote function for generating readme

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description for your project:'
+      },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
